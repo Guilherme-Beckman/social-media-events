f169d7995d3b569ac4c3bbcd25d56e0ef64588ac
Guilherme-Beckman <guilhermebeckman3@gmail.com>
2024-10-06 16:14:05 -0400

    feat: add a background pattern to login-register page

--- frontend/social-media-events/src/app/app.component.ts
+++ frontend/social-media-events/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DefaultLayoutPageLoginRegisterComponent } from "./components/default-layout-page-login-register/default-layout-page-login-register.component";
 
 @Component({
   selector: 'app-root',
