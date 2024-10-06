380df857f3c8d7df6ff2a51feb03fd0f4de8b1ce
Guilherme-Beckman <guilhermebeckman3@gmail.com>
2024-10-06 13:09:53 -0400

    feat: create a new component to default-layout

---
+++ frontend/social-media-events/src/app/components/default-layout-page-login-register/default-layout-page-login-register.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-default-layout-page-login-register',
+  standalone: true,
+  imports: [],
+  templateUrl: './default-layout-page-login-register.component.html',
+  styleUrl: './default-layout-page-login-register.component.css'
+})
+export class DefaultLayoutPageLoginRegisterComponent {
+
+}
