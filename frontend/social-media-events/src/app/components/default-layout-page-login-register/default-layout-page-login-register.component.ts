94dd906c973b656566d544a6377786d82d34a51a
Guilherme-Beckman <guilhermebeckman3@gmail.com>
2024-10-10 14:09:39 -0400

    feat finish the login page

--- frontend/social-media-events/src/app/components/default-layout-page-login-register/default-layout-page-login-register.component.ts
+++ frontend/social-media-events/src/app/components/default-layout-page-login-register/default-layout-page-login-register.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-default-layout-page-login-register',
@@ -8,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrl: './default-layout-page-login-register.component.css'
 })
 export class DefaultLayoutPageLoginRegisterComponent {
-
+  @Input() title: string ="";
+  @Input() label1: string = "";
+  @Input() label2: string = "";
+  @Input() underLineText: string = "";
+  @Input() primaryButtonText: string = "";
+  @Input() h4Text: string = "";
+  @Input() h4TextSpan: string = "";
 }
