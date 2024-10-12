6f587a7910414a9284ebd2f5716a1a06e52df91f
Guilherme-Beckman <guilhermebeckman3@gmail.com>
2024-10-12 11:14:51 -0400

    create a proguess-steps component

---
+++ frontend/social-media-events/src/app/components/proguess-steps/proguess-steps.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-proguess-steps',
+  standalone: true,
+  imports: [],
+  templateUrl: './proguess-steps.component.html',
+  styleUrl: './proguess-steps.component.css'
+})
+export class ProguessStepsComponent {
+
+}
