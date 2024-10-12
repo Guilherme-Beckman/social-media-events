17856918f1b520579dce6fb526089179d035545f
Guilherme-Beckman <guilhermebeckman3@gmail.com>
2024-10-12 17:52:21 -0400

    feat: integrates the gender select component into the registration page

--- frontend/social-media-events/src/app/pages/register/register.component.ts
+++ frontend/social-media-events/src/app/pages/register/register.component.ts
@@ -1,14 +1,15 @@
-import { Component, Input, Output } from '@angular/core';
+import { Component } from '@angular/core';
 import { ProguessStepsComponent } from '../../components/proguess-steps/proguess-steps.component';
 import { CommonModule } from '@angular/common';
+import { GenderSelectComponent } from '../../components/gender-select/gender-select.component';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [ProguessStepsComponent, CommonModule],
+  imports: [ProguessStepsComponent, CommonModule, GenderSelectComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-  currentStep=2;
+  currentStep=0;
 }
