2080c1ba4e791ef4b97a496f86bd7bc59dfd0d4e
Guilherme-Beckman <guilhermebeckman3@gmail.com>
2024-10-12 16:25:25 -0400

    feat: creat a gender select component

---
+++ frontend/social-media-events/src/app/components/gender-select/gender-select.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gender-select',
+  standalone: true,
+  imports: [],
+  templateUrl: './gender-select.component.html',
+  styleUrl: './gender-select.component.css'
+})
+export class GenderSelectComponent {
+
+}
