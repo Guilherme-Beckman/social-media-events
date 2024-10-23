c53ff61768f9a12c21bce5e64b40a02aa8ff2491
Guilherme-Beckman <guilhermebeckman3@gmail.com>
2024-10-23 18:58:44 -0400

    feat: create a global config server project

---
+++ backend/microservices/global-config/src/main/java/com/ms/global_config/GlobalConfigApplication.java
@@ -0,0 +1,13 @@
+package com.ms.global_config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobalConfigApplication.class, args);
+	}
+
+}
