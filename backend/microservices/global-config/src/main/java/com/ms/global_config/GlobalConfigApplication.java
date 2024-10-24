e45de76b6356bdc98c03bf561da6b3d2d3536eb3
Guilherme-Beckman <guilhermebeckman3@gmail.com>
2024-10-23 20:57:48 -0400

    feat: create a config server using a github repository to storage the files

--- backend/microservices/global-config/src/main/java/com/ms/global_config/GlobalConfigApplication.java
+++ backend/microservices/global-config/src/main/java/com/ms/global_config/GlobalConfigApplication.java
@@ -1,9 +1,11 @@
 package com.ms.global_config;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
+@EnableConfigServer
 public class GlobalConfigApplication {
 
 	public static void main(String[] args) {
